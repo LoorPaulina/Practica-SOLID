a41943a76159ed813aef682f6fc7d42a81183155
Stefano <stesherr@espol.edu.ec>
2022-10-27 13:14:15 -0500

    Solucion completa a la violacion del principio SRP en la clase Pago

--- Práctica SOLID/src/Factura.java
+++ Práctica SOLID/src/Factura.java
@@ -1,4 +1,8 @@
 
 public class Factura {
-
+	//atributos aqui
+	
+	public void calcularImpuesto() {
+		//caclula los impuestos asociados a la compra
+	}
 }
--- Práctica SOLID/src/Pago.java
+++ Práctica SOLID/src/Pago.java
@@ -1,14 +1,9 @@
 
 public class Pago {
 	//atributos aqui
+	
 	public void realizarCobro(double monto) {
 		//cargar el monto de compra al medio de pago
 	}
 	
-	public void calcularImpuestoFactura() {
-		//caclula los impuestos asociados a la compra
-	}
-	public void generarFactura() {
-		//generar nueva Factura
-	}
 }
