4ed85422d504c3fcefd601d1f6772712b4b91276
PaulaPeraltaA <paupagui@espol.edu.ec>
2022-10-27 21:14:02 -0500

    Ejemplo del principio LSP

--- Práctica SOLID/src/PagoPayPal.java
+++ Práctica SOLID/src/PagoPayPal.java
@@ -1,12 +1,9 @@
 
-public class PagoPayPal {
-	private boolean loggedIn; //conexion a cuenta PayPal
+public class PagoPayPal extends PagoElectronico{
+	//private boolean loggedIn; //conexion a cuenta PayPal
 	@Override
 	public void realizarCobro(double monto) {
-		if(!loggedIn) {
-			return;
-		}
-		
+
 		//Cargar el monto de compra al medio de pago
 	}
 }
